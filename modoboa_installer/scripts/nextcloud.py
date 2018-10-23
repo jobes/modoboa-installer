da00b554e38bc59f2775e4eaadb22949b880c671
jobes <jobes666@gmail.com>
2018-10-23 12:31:11 +0200

    fix nectloud install

--- modoboa_installer/scripts/nextcloud.py
+++ modoboa_installer/scripts/nextcloud.py
@@ -60,13 +60,8 @@ class Nextcloud(base.Installer):
     def post_run(self):
         uid = pwd.getpwnam("www-data").pw_uid
         gid = grp.getgrnam("www-data").gr_gid
-        for root, dirs, files in os.walk(self.config.get("nextcloud", "installpath")+'/nextcloud'):
-            for momo in dirs:  
-                os.chown(os.path.join(root, momo), uid, gid)
-            for momo in files:
-                os.chown(os.path.join(root, momo), uid, gid)
 
-
+        subprocess.Popen(("chown -R www-data:www-data "+self.config.get("nextcloud", "installpath")+"/nextcloud/").split())
         subprocess.Popen(('sudo -u www-data php '+self.config.get("nextcloud", "installpath")+'/nextcloud/occ  maintenance:install --database '+\
             self.config.get("database", "engine")+' --database-name '+self.config.get("nextcloud", "dbname")+' --database-user '+\
             self.config.get("nextcloud", "dbuser")+' --database-pass "'+self.config.get("nextcloud", "dbpassword")+\
